881546d39414d545563d9d7a758bf55969c23154
zouro2 <robin.zou@novartis.com>
2017-09-19 15:24:49 +0800

    rob commit for V2.3, for linux

--- service_regist/views.py
+++ service_regist/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 from script_auto_identify import auto_identify_py, auto_identify_r
 
 import json
-import os
+
+service_url_root = r'http://127.0.0.1:8022'
+
 
 # Create your views here.
 def script_to_service(request, stype, rtype, catg_name, app_name, script_name):
@@ -14,7 +16,7 @@ def script_to_service(request, stype, rtype, catg_name, app_name, script_name):
             arg_of_script = auto_identify_r(catg_name, app_name, script_name)
         query_arg_dict = dict(zip(arg_of_script['Arg_Name'], arg_of_script['Arg_Value']))
         query_arg_str = '&'.join('='.join((k, str(v))) for k,v in query_arg_dict.items())
-        service_demo = 'http://127.0.0.1:8080/runservice/%s/%s/%s/%s' % (stype, catg_name, app_name, query_arg_str)
+        service_demo = {'host_url': '%s/runservice/%s/%s/%s/%s' % (service_url_root, stype, catg_name, app_name, query_arg_str)}
 
     else:
         if stype == 'python':
@@ -23,7 +25,7 @@ def script_to_service(request, stype, rtype, catg_name, app_name, script_name):
             arg_of_script = auto_identify_r(catg_name, app_name, script_name)
         query_arg_dict = dict(zip(arg_of_script['Arg_Name'], arg_of_script['Arg_Value']))
 
-        service_host_url = 'http://127.0.0.1:8080/runservice/%s/%s/%s/' % (stype, catg_name, app_name)
+        service_host_url = '%s/runservice/%s/%s/%s/' % (service_url_root, stype, catg_name, app_name)
         service_query_data = json.dumps(query_arg_dict)
         service_demo = {'host_url': service_host_url, 'query_data': service_query_data}
-    return render(request, 'service_regist/show_service_demo.html', {'service_demo': service_demo, 'rtype': rtype})
+    return JsonResponse(service_demo, safe=False)
