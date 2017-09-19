74ad45c9d1c37039507e607917360c153716b21e
zouro2 <robin.zou@novartis.com>
2017-09-19 10:20:25 +0800

    rob commit for V2.3, 0917

--- service_regist/views.py
+++ service_regist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from script_auto_identify import auto_identify_py, auto_identify_r
 
@@ -6,6 +8,7 @@ import json
 import os
 
 # Create your views here.
+@csrf_exempt
 def script_to_service(request, spath, stype, rtype):
     catgname = os.path.basename(os.path.dirname(os.path.split(spath)[0]))
     appname = os.path.basename(os.path.split(spath)[0])
@@ -16,7 +19,7 @@ def script_to_service(request, spath, stype, rtype):
             arg_of_script = auto_identify_r(spath)
         query_arg_dict = dict(zip(arg_of_script['Arg_Name'], arg_of_script['Arg_Value']))
         query_arg_str = '&'.join('='.join((k, str(v))) for k,v in query_arg_dict.items())
-        service_demo = 'http://127.0.0.1:8080/runservice/%s/%s/%s/%s' % (stype,catgname,appname, query_arg_str)
+        service_demo = {'host_url': 'http://127.0.0.1:8080/runservice/%s/%s/%s/%s' % (stype,catgname,appname, query_arg_str)}
 
     else:
         if stype == 'python':
@@ -28,4 +31,5 @@ def script_to_service(request, spath, stype, rtype):
         service_host_url = 'http://127.0.0.1:8080/runservice/%s/%s/%s/' % (stype, catgname, appname)
         service_query_data = json.dumps(query_arg_dict)
         service_demo = {'host_url': service_host_url, 'query_data': service_query_data}
-    return render(request, 'service_regist/show_service_demo.html', {'service_demo': service_demo, 'rtype': rtype})
+    return JsonResponse(service_demo, safe=False)
+    # return render(request, 'service_regist/show_service_demo.html', {'service_demo': service_demo, 'rtype': rtype})
